9e381a45d44fcf5da103c28d144bd8a6a0921f0e
sarmini1 <spencerarmini@gmail.com>
2022-04-15 10:11:38 -0700

    completed second problem in split squares

--- split-squares/split-squares.js
+++ split-squares/split-squares.js
@@ -34,3 +34,76 @@ console.log("first test", test1);
 
 let test2 = dump([0, 0, 0, [1, 1, 1, 1]]) // 0 0 0 1 1 1 1
 console.log("second test", test2);
+
+let test3 = dump([0, 0, 0, [1, 1, 1, [0, 0, 0, [1, 1, 1, 1]]]]);
+console.log("test3", test3); // 0 0 0 1 1 1 0 0 0 1 1 1 1
+
+
+// validate(0) // true
+// A split square of four simple filled squares is valid:
+
+// validate([1, 1, 1, 1]) // true
+
+// validate(2) // false
+
+// validate([1, 1, 1, 1, 1]) // false
+
+// validate([1, 0, [1, [0, 0, 0, 0, 1], 1, [1, 1, 1, 1]], 1]) // false
+
+// validate([1, [1, 0, 1, [0, [0, 0, 0], 1, 1]], [1, 0, 1, 0], 1]) // false
+
+
+// Function takes a square/split square, and returns true if it is valid,
+// and false if it is not.
+function validate(square) {
+
+  //base case
+  if (typeof (square) === "number") {
+    return (square === 0 || square === 1);
+  }
+
+  //progress, if square is an array
+  // loop through array and for each subsquare,
+  // call validate
+
+  if (square.length !== 4) {
+    return false;
+  }
+
+  for (let subsquare of square) {
+
+    let result = validate(subsquare);
+    if (!result) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let test1 = validate(0); // true
+console.log("true", test1);
+
+let test2 = validate([1, 1, 1, 1]) // true
+console.log("true", test2);
+
+let test3 = validate(2) // false
+console.log("false", test3);
+
+let test4 = validate([1, 1, 1, 1, 1]) // false
+console.log("false", test4);
+
+let test5 = validate([1, 0, [1, [0, 0, 0, 0, 1], 1, [1, 1, 1, 1]], 1]) // false
+console.log("false", test5);
+
+let test6 = validate([1, [1, 0, 1, [0, [0, 0, 0], 1, 1]], [1, 0, 1, 0], 1]) // false
+console.log("false", test6);
+
+let test7 = validate([1, 0, [1, [0, 0, 0, 0], 1, [1, 1, 1, 1]], 1]); // true
+console.log("true 7", test7);
+
+let test8 = validate([1,
+         [1, 0, 1, [0, [0, 0, 0, 0], 1, 1]],
+         [1, 0, 1, 0],
+         1]);  // true
+console.log("true test 8", test8);
