862a474f2eabb2850402f32e43618ac60ee4a82c
sarmini1 <spencerarmini@gmail.com>
2022-04-16 20:17:31 -0700

    working on simplify function, still debugging

--- split-squares/split-squares.js
+++ split-squares/split-squares.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 /**
  * Given a square, represented as either a plain integer
  * or in the case of a split square, an array of integers,
@@ -27,8 +26,9 @@ function dump(square) {
 }
 
 
-// Function takes a square/split square, and returns true if it is valid,
-// and false if it is not.
+/** Function takes a square/split square, and returns true if it is valid,
+ * and false if it is not.
+ */
 function validate(square) {
 
   if (typeof (square) === "number") {
@@ -55,10 +55,103 @@ function validate(square) {
   return true;
 }
 
+/** Takes in a square, could be an integer or an array,
+ *  and returns the simplified version of it, also either an integer
+ *  or an array.
+ *
+ * simplify([1, 1, 1, 1])  // 1
+ * simplify([0, 0, 0, 0])   // 0
+ * simplify([1, 0, 1, 0])  // [1, 0, 1, 0]
+ * simplify([1, 0, 1, [1, 1, 1, 1]])  // [1, 0, 1, 1]
+ *
+ * @param {*} square
+ */
+function simplify(square) {
+
+  if (typeof (square) === "number") {
+    return square;
+  }
+
+  let simpleSquares = [];
+
+  // we know square can now be simplified
+  // loop through each subsquare
+  for (let subsquare of square) {
+
+    if (Array.isArray(subsquare)){
+      let simplified = simplify(subsquare);
+      subsquare = simplified;
+    }
+
+    // if the current subsquare is just an integer
+    // add it to smaller array of subsquares so we can
+    // check if it's a mixed square or not
+    if (typeof (subsquare) === "number") {
+      simpleSquares.push(subsquare);
+
+      if (simpleSquares.length === 4) {
+        let uniqueVals = new Set(simpleSquares);
+        // if we have a mixed square, we can't simplify it
+        // need to push this to the larger simpleSquares array
+        if (uniqueVals.size !== 1) {
+          return simpleSquares;
+        }
+        else {
+          return subsquare;
+        }
+      }
+      continue;
+    }
+
+    // if subsquare is an ARRAY
+    // we need to recurse and call simplify
+
+    // if (typeof (simplified) === "number") {
+    //   return simplified;
+    // }
+    // simpleSquares.push(simplified);
+  }
+  console.log("simplesquare return val", simpleSquares);
+  return simpleSquares;
+
+}
 
 
 
 module.exports = {
   dump,
-  validate
+  validate,
+  simplify
 };
+
+
+// if (typeof(square) === "number"){
+//   return square;
+// }
+
+// let simpleSquares = [];
+
+ //if square is an array
+// // first check that array is not a mixed square
+// // let uniqueSquareVals = new Set(square);
+// // console.log(uniqueSquareVals);
+// // if (uniqueSquareVals.size !== 1){
+// //   //mixed squares cannot be simplified-- just return the square
+// //   return square;
+// // }
+
+// console.log("above for loop");
+
+// // we know square can now be simplified
+// // loop through each subsquare
+// for (let subsquare of square){
+//   console.log("square is ", square);
+//   let simplified = simplify(subsquare);
+
+//   if (typeof(simplified) === "number"){
+//     return simplified;
+//   }
+//   simpleSquares.push(simplified);
+// }
+
+// return simpleSquares;
--- split-squares/split-squares.test.js
+++ split-squares/split-squares.test.js
@@ -2,7 +2,8 @@
 
 const {
   dump,
-  validate
+  validate,
+  simplify
 } = require("./split-squares");
 
 describe("dump", function () {
@@ -39,6 +40,30 @@ describe("validate", function () {
     expect(validate([1, 1, 1, 1, 1])).toEqual(false);
     expect(validate([1, 0, [1, [0, 0, 0, 0, 1], 1, [1, 1, 1, 1]], 1])).toEqual(false);
     expect(validate([1, [1, 0, 1, [0, [0, 0, 0], 1, 1]], [1, 0, 1, 0], 1])).toEqual(false);
+
+  });
+});
+
+describe("simplify", function () {
+  it("works with simple squares", function () {
+    expect(simplify(0)).toEqual(0);
+  });
+
+  it("works with split squares", function () {
+    // expect(simplify([1, 1, 1, 1])).toEqual(1);
+    // expect(simplify([0, 0, 0, 0])).toEqual(0);
+    // expect(simplify([1, 0, 1, 0])).toEqual([1, 0, 1, 0]);
+    // expect(simplify([1, 0, 1, [1, 1, 1, 1]])).toEqual([1, 0, 1, 1]);
+    // expect(simplify([1, 1, 1, [1, 1, 1, 1]])).toEqual(1);
+    // expect(simplify([[1, 1, 1, 1], [1, 1, 1, 1], 1, 1])).toEqual(1);
+    expect(
+      simplify(
+        [1, 0, [1, [0, 0, 0, 0], 1, [1, 1, 1, 1]], 1]
+      )
+    ).toEqual(
+      [1, 0, [1, 0, 1, 1], 1]
+    );
+
 
   });
 });
